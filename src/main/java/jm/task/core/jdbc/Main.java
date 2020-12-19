2ab40ad712353bf1f8d5dc7715877fbc95685d50
AntonAMorozov <cage_crew@mail.ru>
2020-12-19 19:03:39 +0300

    edit main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -11,9 +11,6 @@ public class Main {
         UserServiceImpl userService = new UserServiceImpl();
         userService.createUsersTable();//1
         userService.saveUser("Bill", "Brown", (byte) 21); //2
-        userService.saveUser("Anton", "Morozov", (byte) 31); //2
-        userService.saveUser("John", "Smith", (byte) 18); //2
-        userService.saveUser("Alice", "Cooper", (byte) 55); //2
         List<User> listOfUsers = userService.getAllUsers();//3
         System.out.println(listOfUsers);//3
         userService.cleanUsersTable();//4
